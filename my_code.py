7ed3e9a225740be46e598e3a8c0ba531aa9755e0
yesak1 <23kkazliner@athenian.org>
2020-10-31 09:13:28 +0000

    UPDATED AFTER FEEDBACK

--- my_code.py
+++ my_code.py
@@ -1,20 +1,21 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #Megan
 teamdictionary = {}
-while True:
-    teams=input("enter an NFL team: ")
-    qbs=input("enter a quarterback: ")
-    teamdictionary[teams]=qbs
-    stop=input("press stop to stop or cont to continue ")
-    if stop=="stop":
-        for x, y in teamdictionary.items():
-            import sys
-            print (teamdictionary)
-            quit()
-    if stop=="cont":
+def code():
+    while True:
         teams=input("enter an NFL team: ")
         qbs=input("enter a quarterback: ")
+        teamdictionary[teams]=qbs
         stop=input("press stop to stop or cont to continue ")
+        if stop=="stop":
+            for x, y in teamdictionary.items():
+                print (teamdictionary)
+                import sys
+                quit()
+    if stop=="cont":
+        code()
+
+code()
 
         
         
